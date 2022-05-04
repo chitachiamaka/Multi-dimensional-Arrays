ad93176fc96fac5c3fe195ef8d95795b9bea2c0b
chitachiamaka <chitachiamaka.github@gmail.com>
2022-05-04 07:12:14 +0530

    1st commit

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
+//Question1
 var arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
 console.log(arr[5][0][0],arr[3],arr[5][0][3][0],arr[5][1],arr[1][0]);
+//Question2
 var sentence = ["javascript", "web", "Capacity Bay"];
 output = `Iam a ${sentence[1]} developer at ${sentence[2]} and I love ${sentence[0]}`;
 console.log(output)
+//Question3
+var word = "Generalization";
+console.log(word.slice(3));
+//Question4
 var fruit = ["orange", "apple", "guava", "lemon"];
 fruit.unshift("pea", "banana");
 fruit.push("Avocado", "mango");
 console.log(fruit)
+//section2
 var arr = [];
 // var count = 1;
 function writeNum() {
